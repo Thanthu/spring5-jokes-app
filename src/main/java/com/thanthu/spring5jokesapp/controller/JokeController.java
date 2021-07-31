6ed0f4871513eb62ec0717179df587e29a734010
Thanthu <tnair@orthofx.com>
2021-07-31 18:12:15 +0530

    Create Joke Controller, Create template to show joke. Closes #4, Closes #5

---
+++ src/main/java/com/thanthu/spring5jokesapp/controller/JokeController.java
@@ -0,0 +1,24 @@
+package com.thanthu.spring5jokesapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.thanthu.spring5jokesapp.services.JokeService;
+
+@Controller
+public class JokeController {
+
+	private final JokeService jokeService;
+
+	public JokeController(JokeService jokeService) {
+		this.jokeService = jokeService;
+	}
+	
+	@RequestMapping({"/", ""})
+	public String showJoke(Model model) {
+		model.addAttribute("joke", jokeService.getJoke());
+		return "index";
+	}
+	
+}
